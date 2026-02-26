35def8c9824c9d9812dda9f4e807528ee38da904
preethihepsiba-cloud <preethi.hepsiba@gmail.com>
2026-02-26 13:49:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,110 +1,63 @@
 import os
-import sqlite3
 import uuid
 from datetime import timedelta
-from flask import Flask, render_template, request, redirect, url_for, g
+from flask import Flask, render_template, request, redirect, url_for
 from werkzeug.utils import secure_filename
 from google.cloud import storage
 
 app = Flask(__name__)
 
-DATABASE = "database.db"
 BUCKET_NAME = os.environ.get("BUCKET_NAME")
 
 storage_client = storage.Client()
 
-# ---------------- DATABASE ----------------
-
-def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = sqlite3.connect(DATABASE)
-        db.row_factory = sqlite3.Row
-    return db
-
-@app.teardown_appcontext
-def close_connection(exception):
-    db = getattr(g, '_database', None)
-    if db is not None:
-        db.close()
-
-def init_db():
-    with app.app_context():
-        db = get_db()
-        db.execute('''
-            CREATE TABLE IF NOT EXISTS students (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                name TEXT NOT NULL,
-                email TEXT NOT NULL,
-                photo_filename TEXT
-            )
-        ''')
-        db.commit()
 
 # ---------------- STORAGE ----------------
 
 def upload_to_bucket(file, filename):
+    if not BUCKET_NAME:
+        raise ValueError("BUCKET_NAME environment variable not set")
+
     bucket = storage_client.bucket(BUCKET_NAME)
     blob = bucket.blob(filename)
+
     blob.upload_from_file(file, content_type=file.content_type)
     return filename
 
-def generate_signed_url(filename):
-    bucket = storage_client.bucket(BUCKET_NAME)
-    blob = bucket.blob(filename)
-    return blob.generate_signed_url(
-        version="v4",
-        expiration=timedelta(minutes=30),
-        method="GET",
-    )
 
 # ---------------- ROUTES ----------------
-
 @app.route('/')
 def index():
-    db = get_db()
-    students = db.execute("SELECT * FROM students").fetchall()
+    bucket = storage_client.bucket(BUCKET_NAME)
+    blobs = bucket.list_blobs()
 
-    student_list = []
-    for student in students:
-        photo_url = None
-        if student["photo_filename"]:
-            photo_url = generate_signed_url(student["photo_filename"])
+    image_urls = []
 
-        student_list.append({
-            "name": student["name"],
-            "email": student["email"],
-            "photo_url": photo_url
-        })
+    for blob in blobs:
+        public_url = f"https://storage.googleapis.com/{BUCKET_NAME}/{blob.name}"
+        image_urls.append(public_url)
 
-    return render_template("index.html", students=student_list)
+    return render_template("index.html", images=image_urls)
+    
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
-        name = request.form['name']
-        email = request.form['email']
-        photo = request.files['photo']
+        photo = request.files.get('photo')
 
-        photo_filename = None
-
-        if photo:
+        if photo and photo.filename != "":
             filename = secure_filename(photo.filename)
             unique_name = str(uuid.uuid4()) + "_" + filename
+
             upload_to_bucket(photo, unique_name)
-            photo_filename = unique_name
 
-        db = get_db()
-        db.execute(
-            "INSERT INTO students (name, email, photo_filename) VALUES (?, ?, ?)",
-            (name, email, photo_filename)
-        )
-        db.commit()
+            # Just redirect to homepage to show thumbnails
+            return redirect(url_for('index'))
 
-        return redirect(url_for('index'))
+        return "No file selected", 400
 
     return render_template("register.html")
 
+
 if __name__ == '__main__':
-    init_db()
     app.run(host='0.0.0.0', port=8080)
